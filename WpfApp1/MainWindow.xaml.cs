cc186f1214ab3ecdea326ede696b5d426dcee2d9
Pieter Schiepers <pieter.schiepers@vistalink.eu>
2020-02-07 10:47:32 +0100

    Add messageSourceDemo to main application

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace WpfApp1
 {
@@ -7,10 +8,16 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IList<messageSourceBase> messageSources = new List<messageSourceBase>();
+
         public MainWindow()
         {
             InitializeComponent();
+
             this.myLabel.Content = null;
+
+            //Register your message sources here
+            messageSources.Add(new messageSourceDemo());
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -18,6 +25,11 @@ namespace WpfApp1
             if (this.myLabel.Content == null)
             {
                 this.myLabel.Content = "Hello world";
+                foreach (messageSourceBase msgSrc in messageSources)
+                {
+                    this.myLabel.Content += "\n";
+                    this.myLabel.Content += msgSrc.GetMessage();
+                }
             }
             else
             {
